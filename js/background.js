c8c4ed13b94a34e0aec7f5d9e2f592667b7a5417
Kero-the-Hero <syashley1119@gmail.com>
2023-12-08 16:40:46 -0800

    background part

---
+++ js/background.js
@@ -0,0 +1,17 @@
+const images = [
+    'Picture1.png',
+    'Picture2.jpg',
+    'Picture3.png',
+    'Picture4.jpg',
+    'Picture5.jpg',
+    'Picture6.png',
+    'Picture7.jpg',
+];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement('img');
+
+bgImage.src = `src/background/${chosenImage}`;
+
+document.body.appendChild(bgImage);
